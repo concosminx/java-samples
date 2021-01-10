3a73239e99b1523ecc13b30e960628b32b49603a
Cosmin Constantin <concosminx@users.noreply.github.com>
2021-01-10 15:22:04 +0200

    add spark sample

---
+++ java_ws/spark_ws/src/main/java/spark_ws/UserService.java
@@ -0,0 +1,22 @@
+package spark_ws;
+
+/**
+ *
+ * @author Cosminx
+ */
+import java.util.Collection;
+
+public interface UserService {
+
+  public void addUser(User user);
+
+  public Collection<User> getUsers();
+
+  public User getUser(String id);
+
+  public User editUser(User user) throws UserException;
+
+  public void deleteUser(String id);
+
+  public boolean userExist(String id);
+}
