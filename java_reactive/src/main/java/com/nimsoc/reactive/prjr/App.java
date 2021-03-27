b81e3c2e9cdeb98a28a227b6e9cf76a6107761dd
Cosmin Constantin <concosminx@users.noreply.github.com>
2021-03-27 13:12:09 +0200

    removed spark ws

---
+++ java_reactive/src/main/java/com/nimsoc/reactive/prjr/App.java
@@ -0,0 +1,11 @@
+package com.nimsoc.reactive.prjr;
+
+public class App {
+
+  public static void main(String[] args) {
+    PublisherExample.fluxSubscriber();
+    PublisherExample.monoSubscriber();
+    PublisherExample.callbacksSementic();
+  }
+
+}
